c5d17c5d440cb0ffd8baf828a444b85511d79743
Gauravz-git <gauravc0223@gmail.com>
2025-05-15 02:08:35 +0530

    Add initial implementation of class_and_objects with main method

---
+++ oops-concepts/class_and_objects.java
@@ -0,0 +1,10 @@
+
+class class_and_objects{ // classname
+    String Name; //class attributes
+    int number;
+
+    public static void main(String[] args){ // main function
+        /* Creating an object using "new" keyboard */
+       class_and_objects obj = new class_and_objects(); 
+    }
+}
