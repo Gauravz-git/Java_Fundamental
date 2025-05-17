2dddd60bce4aff3b1d38ee842cacd20f9fe17ba6
Gauravz-git <gauravc0223@gmail.com>
2025-05-17 21:24:37 +0530

    Add method to print greeting and call it in main function

--- oops-concepts/class_and_objects.java
+++ oops-concepts/class_and_objects.java
@@ -3,8 +3,13 @@ class class_and_objects{ // classname
     String Name; //class attributes
     int number;
 
+    public void integer(){
+        System.out.println("Hello World");
+    }
+
     public static void main(String[] args){ // main function
         /* Creating an object using "new" keyboard */
        class_and_objects obj = new class_and_objects(); 
+       obj.integer(); // call function with object
     }
 }
